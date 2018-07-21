0a2fd5308e2141e8662a01992201d28c5c06367a
tyleryoungblood <tyler@resimplifi.com>
2018-07-21 15:05:44 -0400

    inject filter dependences into FilmApp module

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-angular.module("FilmApp", []).controller("FilmCtrl", [
-  "$scope",
-  "$http",
-  function($scope, $http) {
-    $http
-      .get("https://data.sfgov.org/resource/wwmu-gmzc.json")
-      .then(function(response) {
-        //console.log(response.data);
-        $scope.films = response.data;
-      });
-  }
-]);
+angular
+  .module("FilmApp", ["angular.filter", "ui.filters"])
+  .controller("FilmCtrl", [
+    "$scope",
+    "$http",
+    function($scope, $http) {
+      $http
+        .get("https://data.sfgov.org/resource/wwmu-gmzc.json")
+        .then(function(response) {
+          //console.log(response.data);
+          $scope.films = response.data;
+        });
+    }
+  ]);
